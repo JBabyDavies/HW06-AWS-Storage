7795eb8cd8a8a8d3fbac3506f4a6a40f1ee1b012
JBabyDavies <64869117+JBabyDavies@users.noreply.github.com>
2021-02-09 12:36:34 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello World, this is a test for Amplify from Github from Jordan
         </p>
         <a
           className="App-link"
